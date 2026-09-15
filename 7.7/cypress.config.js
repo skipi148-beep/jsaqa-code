cc210173a32619056f483463b102b5fa169a5981
Skipi <skipi148@gmail.com>
2026-09-15 22:35:53 +0300

    Добавил projectId в конфигурацию 7.7

--- 7.7/cypress.config.js
+++ 7.7/cypress.config.js
@@ -1,1 +1,10 @@
-const { defineConfig } = require('cypress'); module.exports = defineConfig({ e2e: { specPattern: 'cypress/integration/**/*.spec.js', supportFile: false, fixturesFolder: 'cypress/fixtures' } });
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    projectId: "69p8u7", // Уникальный ID проекта для Cypress Cloud
+    specPattern: "cypress/integration/**/*.spec.js",
+    supportFile: false,
+    fixturesFolder: "cypress/fixtures"
+  },
+});
