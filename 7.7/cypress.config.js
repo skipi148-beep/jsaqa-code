7fda076b8d56ac5dc0cff21e978fbb146964cb18
Skipi <skipi148@gmail.com>
2026-09-15 22:38:42 +0300

    Принудительно добавил projectId в конфиг 7.7

--- 7.7/cypress.config.js
+++ 7.7/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "69p8u7", // Уникальный ID проекта для Cypress Cloud
+    projectId: "69p8u7", // ID для интеграции с Cypress Cloud Dashboard
     specPattern: "cypress/integration/**/*.spec.js",
     supportFile: false,
     fixturesFolder: "cypress/fixtures"
