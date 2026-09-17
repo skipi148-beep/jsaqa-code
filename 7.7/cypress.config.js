5849ecbac37816abb651b909ab32ecb7739f9724
Skipi <skipi148@gmail.com>
2026-09-17 22:40:05 +0300

    fix: final green version of ui and api tests

--- 7.7/cypress.config.js
+++ 7.7/cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://qamid.tmweb.ru", // <-- Возвращаем оригинальный URL проекта
+    projectId: "jg63d4", 
+    baseUrl: "http://qamid.tmweb.ru",
     supportFile: false,
     specPattern: "cypress/integration/**/*.spec.js"
   },
--- 7.7/cypress/integration/api.spec.js
+++ 7.7/cypress/integration/api.spec.js
@@ -1,6 +1,6 @@
 describe('API тесты - Проверка сервера кинотеатра', () => {
   it('1. Проверка доступности главной страницы клиента (GET)', () => {
-    // Вместо полного URL пишем просто косую черту — Cypress сам подставит http://localhost:8080
+    
     cy.request('/').then((response) => {
       expect(response.status).to.eq(200);
     });
